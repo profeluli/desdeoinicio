d7d90dce150279fec09f2817e094d6700206bc50
profeluli <115101305+profeluli@users.noreply.github.com>
2024-05-11 11:00:05 -0300

    432342342342342342

--- main.js
+++ main.js
@@ -1,2 +1,55 @@
 //elementos capturados do HTML
 const divRelogio = document.querySelector('.relogio');
+
+// Aula 11/05
+let numeroAleatorio;
+
+numeroAleatorio = Math.floor(Math.random() * 10);  // colocar um número inteiro dentro dos aleatórios
+
+console.log(`Número aleatório: ${numeroAleatorio}`); 
+
+let numeroPiso;
+
+numeroPiso = Math.floor(4.87);
+console.log(`Número piso: ${numeroPiso}`);
+
+const dataInicial = new Date(); //sem parâmetro, retorna a data atual.
+console.log(dataInicial);
+console.log(dataInicial.getHours());
+console.log(dataInicial.getMonth());
+
+
+// Para criar um relógio que atualize linha a linha:
+function atualizaRelogio() {
+    const dataAtual = new Date();
+    let horas = dataAtual.getHours();
+    horas = adicionaZero(horas);
+    let minutos = dataAtual.getMinutes();
+    minutos = adicionaZero(minutos);
+    let segundos = dataAtual.getSeconds();
+    segundos = adicionaZero(segundos);
+    
+    console.log(`${horas}:${minutos}:${segundos}`);
+    divRelogio.innerHTML = (`${horas}:${minutos}:${segundos}`);
+    
+}
+
+atualizaRelogio();
+
+const relogio = setInterval(function(){
+    atualizaRelogio();
+}, 1000);
+
+setTimeout(function(){
+    clearInterval(relogio);
+}, 10000);
+
+//customizando com o zero à esquerda
+function adicionaZero(numero){
+    if(numero < 10){
+        numero = "0"+numero;
+        }
+        return numero;
+}
+
+adicionaZero();
